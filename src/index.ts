d23500d12c4bf8bc7f36c576ab820547a0a22d32
My Name <walker.b.lobato@gmail.com>
2023-01-10 22:12:32 -0300

    feat: add folders struture

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import{ server } from './server/Server';
 
 
-server.listen(3000, () => console.log('App rodando!'));
+server.listen(process.env.PORT || 3000, () => console.log(`App rodando na porta ${process.env.PORT || 3000}!`));
