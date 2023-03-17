3bc679fa1ce04797c9acfea6d820cb50e51a3ef1
My Name <walker.b.lobato@gmail.com>
2023-03-17 00:36:29 -0300

    feat: add seed to cities

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
 import{ server } from './server/Server';
+import { Knex } from './server/database/knex';
 
 
-server.listen(process.env.PORT || 3000, () => console.log(`App rodando na porta ${process.env.PORT || 3000}!`));
+const startServer = () => {
+    server.listen(process.env.PORT || 3000, () => {
+        console.log(`App rodando na porta ${process.env.PORT || 3000}!`);
+    });
+};
+
+if (process.env.IS_LOCALHOST !== 'true') {
+    Knex.migrate
+        .latest()
+        .then(() => {
+            Knex.seed.run()
+                .then(() => startServer())
+                .catch(console.log);
+        })
+        .catch(console.log);
+} else {
+    startServer();
+}
